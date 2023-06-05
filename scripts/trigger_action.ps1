9e95d073ba87dc12835e9834259d5c2dfa0a0194
Jared Holgate <jaredholgate@microsoft.com>
2023-06-05 23:17:49 +0100

    Fix pipeline

--- scripts/trigger_action.ps1
+++ scripts/trigger_action.ps1
@@ -5,29 +5,6 @@ param(
     [string]$subscriptionData
 )
 
-$owner = "jared-holgate-microsoft-demos"
-$repository = "terraform-jaredfholgate-azure-subscription-vending-demo"  
-$access_token = "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"
-$subscriptionData = @"
-{
-    "subscription_business_unit": "it",
-    "subscription_purpose": "sandbox",
-    "subscription_number": 3,
-    "location": "uksouth",
-    "subscription_offer": "DevTest",
-    "subscription_description": "A sandbox subscription for IT to use for testing.",
-    "subscription_management_group": "Subscription Vending Demo",
-    "billing_account_name": "7690848",
-    "billing_enrollment_name": "340368",
-    "create_service_principal": true,
-    "create_repository": true,
-    "create_terraform_cloud_workspace": true,
-    "subscription_owners": [
-        "jaredholgate_microsoft.com#EXT#"
-    ]
-}
-"@
-
 $headers=@{
   "Authorization" = "Bearer $access_token"
   "X-GitHub-Api-Version" = "2022-11-28"
