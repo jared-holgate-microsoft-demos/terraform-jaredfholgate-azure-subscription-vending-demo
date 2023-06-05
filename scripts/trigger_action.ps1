01010290fef87df7b499e396bde5bf6799bc5aa4
Jared Holgate <jaredholgate@microsoft.com>
2023-06-05 23:13:27 +0100

    Fix limit of 10 variables

--- scripts/trigger_action.ps1
+++ scripts/trigger_action.ps1
@@ -2,9 +2,31 @@ param(
     [string]$access_token,
     [string]$owner,
     [string]$repository,
-    [string]$event_type,
     [string]$subscriptionData
 )
+
+$owner = "jared-holgate-microsoft-demos"
+$repository = "terraform-jaredfholgate-azure-subscription-vending-demo"  
+$access_token = "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"
+$subscriptionData = @"
+{
+    "subscription_business_unit": "it",
+    "subscription_purpose": "sandbox",
+    "subscription_number": 3,
+    "location": "uksouth",
+    "subscription_offer": "DevTest",
+    "subscription_description": "A sandbox subscription for IT to use for testing.",
+    "subscription_management_group": "Subscription Vending Demo",
+    "billing_account_name": "7690848",
+    "billing_enrollment_name": "340368",
+    "create_service_principal": true,
+    "create_repository": true,
+    "create_terraform_cloud_workspace": true,
+    "subscription_owners": [
+        "jaredholgate_microsoft.com#EXT#"
+    ]
+}
+"@
 
 $headers=@{
   "Authorization" = "Bearer $access_token"
@@ -16,8 +38,11 @@ $subscriptionVariables = ConvertFrom-Json $subscriptionData
 $url = "https://api.github.com/repos/$owner/$repository/dispatches"
 $body=@{
   "event_type" = "vend_subscription"
-  "client_payload" = $subscriptionVariables
+  "client_payload" = @{
+    "subscriptionData" = $subscriptionVariables
+  }
 }
-$bodyJson = ConvertTo-Json $body
+$bodyJson = ConvertTo-Json $body -Depth 10
 
 $result = Invoke-RestMethod -Method "POST" -Uri $url -Body $bodyJson -Headers $headers -ContentType "application/vnd.github+json"
+
